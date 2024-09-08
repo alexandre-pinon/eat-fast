6e50586625de9f51c1d3e2adce486668bae3cf37
Alexandre Pinon <apinon0205@gmail.com>
2024-09-08 14:18:23 +0200

    fix: logo link flex

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Logo = ({ size }: { size: number | `${number}` }) => {
   return (
-    <Link color="foreground" className="items-center gap-x-2" href="/">
+    <Link color="foreground" className="flex items-center gap-x-2" href="/">
       <Image src="/logo.svg" alt="logo" width={size} height={size} />
       <p className="font-semibold text-lg">EatFast</p>
     </Link>
